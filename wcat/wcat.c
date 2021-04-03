af2c33dcaa85726f6ca607c13e035e9ad4fb1251
luiz <luizgustavofcsg@hotmail.com>
2021-04-02 21:25:30 -0300

    basic functionality implementation

--- wcat/wcat.c
+++ wcat/wcat.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+long get_file_length(FILE **file) {
+	fseek((*file), 0, SEEK_END);
+	long length = ftell(*file);
+	fseek((*file), 0, SEEK_SET);
+
+	return length;
+}
 
 int validate_input(int argc, char **argv) {
 	
@@ -18,6 +27,15 @@ void read_content(char *filename) {
 		printf("ERROR: Could not find file: %s\n", filename);
 	}
 
+	long length = get_file_length(&file);
+
+	char *buffer = malloc(length * sizeof(char));
+	if (buffer != NULL) {
+		fread(buffer, 1, length, file);
+		printf("%s", buffer);
+	}
+
+	fclose(file);
 }
 
 int main(int argc, char *argv[]) {
