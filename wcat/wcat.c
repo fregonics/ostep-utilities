6cd051dcda7ac1f04941c8f15740fc479ae993bd
luiz <luizgustavofcsg@hotmail.com>
2021-04-02 21:07:55 -0300

    argument validation

--- wcat/wcat.c
+++ wcat/wcat.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-	printf("%d\n", argc);
+int validate_input(int argc, char **argv) {
 	
-	int i = 0;
-	while(argv[i] != NULL) {
-		printf("%s\n", argv[i]);
-		i ++;
+	if(argc < 2) {
+		printf("You must enter a file to be read.\n");
+		return 0;
 	}
-	
+
+	return 1;
+
+}
+
+void read_content(char *filename) {
+	FILE *file = fopen(filename, "r");
+
+	if (file == NULL) {
+		printf("ERROR: Could not find file: %s\n", filename);
+	}
+
+}
+
+int main(int argc, char *argv[]) {
+
+	if( !validate_input(argc, argv) ) {
+		return 0;
+	}
+
+	read_content(argv[argc - 1]);
+
 	return 0;
 }
